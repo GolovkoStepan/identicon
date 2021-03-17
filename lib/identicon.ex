c4e07077ad46fffa6c62327871eceb3173708220
s.golovko <s.golovko@fun-box.ru>
2021-03-18 00:01:48 +0500

    Create main logic.

---
+++ lib/identicon.ex
@@ -0,0 +1,74 @@
+defmodule Identicon do
+  def main(str) do
+    str
+    |> hash_str
+    |> peek_color
+    |> build_grid
+    |> filter_odd_elems
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(str)
+  end
+
+  def save_image(binary, filename) do
+    File.write("#{filename}.png", binary)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill  = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_value, index}) ->
+      horizontal   = rem(index, 5) * 50
+      vertical     = div(index, 5) * 50
+      top_left     = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_elems(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({value, _index}) -> 
+      rem(value, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def hash_str(str) do
+    hex = :crypto.hash(:md5, str)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def peek_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+end
